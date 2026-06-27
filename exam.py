659b7e343b1e157b89646e8a3da7f3d7f51ed32c
kushal <nishadlavkush129@gmail.com>
2026-06-27 12:59:43 +0530

    Add files via upload

---
+++ exam.py
@@ -0,0 +1,22 @@
+print(" welcome to the bill splitter app !")
+
+total= float(input("enter total bill amount:"))
+ people= int(input("enter number of people:"))
+ tip= float(input("enter tip percentage":))
+  tip percentage=float(input("enter percentage"))  
+    
+
+ print ("tip amount":)
+     enter int(input("enter your tip amount":))
+ print ("total bill(with tip)":)
+ print ("each person should pay":)
+
+
+
+
+
+
+
+
+
+
